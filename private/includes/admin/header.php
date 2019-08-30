b57a52791362c3854445870af03b0a9652ae525d
Suman Das <sumandas.rds@gmail.com>
2019-08-30 09:14:32 +0530

    2nd commit

--- private/includes/admin/header.php
+++ private/includes/admin/header.php
@@ -5,9 +5,7 @@
   <title>Admin -  <?php if(isset($page_title)) echo ' - '.$page_title; ?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  
   <link rel="stylesheet" href="/portfolio/public/css/freelancer.css">
 </head>
 <body>
