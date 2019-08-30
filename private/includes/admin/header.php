1631e1b3bcb15ef30dce7298da1aae06aa958df2
Suman Das <sumandas.rds@gmail.com>
2019-08-30 08:58:13 +0530

    first

---
+++ private/includes/admin/header.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Admin -  <?php if(isset($page_title)) echo ' - '.$page_title; ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/portfolio/public/css/freelancer.css">
+</head>
+<body>
---
+++ public/admin/about/index.php
@@ -0,0 +1,7 @@
+<?php require_once('../../../private/initialize.php'); ?>
+<?php include(INCLUDE_PATH_ADM.'/header.php'); ?>
+
+<button type="button" class="btn btn-warning" name="Edit"></button>
+<?php include(INCLUDE_PATH_PUB.'/about.php'); ?>
+
+<?php include(INCLUDE_PATH_ADM.'/footer.php'); ?>
